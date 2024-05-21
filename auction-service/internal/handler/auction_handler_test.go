77ef64a49d723a268b240c6b7eb183ffebedcc64
Renato <renatogp13@gmail.com>
2024-05-21 10:33:01 +0200

    added update delete test for handler

--- auction-service/internal/handler/auction_handler_test.go
+++ auction-service/internal/handler/auction_handler_test.go
@@ -99,3 +99,94 @@ func TestGetAuctionByID(t *testing.T) {
 
 	mockRepo.AssertExpectations(t)
 }
+func TestUpdateAuction(t *testing.T) {
+	mockRepo := new(MockAuctionRepository)
+	initialAuction := model.Auction{ID: 1, Item: "Initial Item", UserID: 1}
+	updatedAuction := model.Auction{ID: 1, Item: "Updated Item", UserID: 1}
+
+	mockRepo.On("CreateAuction", initialAuction).Return(initialAuction, nil)
+
+	mockRepo.On("UpdateAuction", updatedAuction).Return(nil)
+
+	auctionHandler := handler.NewAuctionHandler(mockRepo)
+
+	createdAuction, err := mockRepo.CreateAuction(initialAuction)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	reqBody, _ := json.Marshal(updatedAuction)
+	req, err := http.NewRequest("PUT", "/auctions/1", bytes.NewBuffer(reqBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(auctionHandler.UpdateAuction)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+
+	assert.NotEqual(t, updatedAuction.Item, createdAuction.Item)
+	assert.Equal(t, updatedAuction.UserID, createdAuction.UserID)
+
+	mockRepo.AssertExpectations(t)
+}
+
+func TestGetAllAuctions(t *testing.T) {
+	mockRepo := new(MockAuctionRepository)
+	auctions := []model.Auction{
+		{Item: "Test Item 1", UserID: 1},
+		{Item: "Test Item 2", UserID: 2},
+	}
+	mockRepo.On("GetAllAuctions").Return(auctions, nil)
+
+	auctionHandler := handler.NewAuctionHandler(mockRepo)
+
+	req, err := http.NewRequest("GET", "/auctions", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(auctionHandler.GetAllAuctions)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+
+	var returnedAuctions []model.Auction
+	err = json.Unmarshal(rr.Body.Bytes(), &returnedAuctions)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, auctions, returnedAuctions)
+	mockRepo.AssertExpectations(t)
+}
+
+func TestDeleteAuction(t *testing.T) {
+	mockRepo := new(MockAuctionRepository)
+	auction := model.Auction{ID: 1, Item: "Test Item", UserID: 1}
+	mockRepo.On("CreateAuction", auction).Return(auction, nil)
+	mockRepo.On("DeleteAuction", auction).Return(nil)
+
+	auctionHandler := handler.NewAuctionHandler(mockRepo)
+
+	_, err := mockRepo.CreateAuction(auction)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest("DELETE", "/auctions/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(auctionHandler.DeleteAuction)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusNoContent, rr.Code)
+
+	mockRepo.AssertExpectations(t)
+}
